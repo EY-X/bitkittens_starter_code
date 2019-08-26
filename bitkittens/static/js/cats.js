6d93bbc6eae26915d95ba7fd39ea453b86807e31
EY-X <e.yosefx128@gmail.com>
2019-08-25 20:47:11 -0400

    API not working

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -1,2 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+    const kittyButton = document.querySelector('.summon-cats')
+    
+
+    kittyButton.addEventListener('click', () => {
+        const kittyRequest = axios.get('http://bitkittens.herokuapp.com')
+        kittyRequest.then(response => {
+            console.log(response.data)
+        })
+        .catch(error => {
+            console.log('ERROR!')
+        })
+    })
 });
